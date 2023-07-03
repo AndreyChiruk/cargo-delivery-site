99f7ae2e86a523daa4fb8fc616f86ea40a31ffd5
Andrey <108932683+AndreyChiruk@users.noreply.github.com>
2023-07-03 19:56:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
-new Swiper('.swiper-container', {
-	spaceBetween: 30,
-	loop: true,
-	breakpoints: {
-  		1240: {
-  			slidesPerView: 3,
-  		},
-  		992: {
-  			slidesPerView: 2,
-  		},
-  		320: {
-  			slidesPerView: 1,
-  		}
-  	},
-	pagination: {
-    	el: '.swiper-pagination',
-    	clickable: true,
-  	},
+$(document).ready(function(){
+	$('.swiper-wrapper').slick({
+		arrows: false,
+		slidesToShow: 3,
+		draggable: true,
+		swipe: true,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1,
+		        infinite: true,
+		        dots: false
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1,
+		        dots: true,
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        dots: true,
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		]
+	});
+});
+$(document).ready(function(){
+	$('.swiper-wrapper1').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+	});
 });
